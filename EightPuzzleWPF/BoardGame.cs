5152e2da5e0fc285a35828ee9e870ec17ee1af1d
CYAN4S_another <sih5214p@naver.com>
2018-10-17 15:50:37 +0900

    Add something

--- EightPuzzleWPF/BoardGame.cs
+++ EightPuzzleWPF/BoardGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EightPuzzleWPF
 {
@@ -35,8 +36,83 @@ namespace EightPuzzleWPF
             return;
         }
 
-        //public moveTile()
+
+        public bool MoveTile(Key dir)
+        {
+            switch (dir)
+            {
+                case Key.Up:
+                    if (HoleRow < _status[0].Count)
+                    {
+                        _status[HoleRow][HoleCol] = _status[HoleRow + 1][HoleCol];
+                        _status[HoleRow + 1][HoleCol] = 0;
+                        HoleRow++;
+                        return true;
+                    }
+                    else
+                        return false;
+
+                case Key.Down:
+                    if (HoleRow < _status[0].Count)
+                    {
+                        _status[HoleRow][HoleCol] = _status[HoleRow + 1][HoleCol];
+                        _status[HoleRow + 1][HoleCol] = 0;
+                        HoleRow++;
+                        return true;
+                    }
+                    else
+                        return false;
+
+                case Key.Left:
+                    if (HoleRow < _status[0].Count)
+                    {
+                        _status[HoleRow][HoleCol] = _status[HoleRow + 1][HoleCol];
+                        _status[HoleRow + 1][HoleCol] = 0;
+                        HoleRow++;
+                        return true;
+                    }
+                    else
+                        return false;
+
+                case Key.Right:
+                    if (HoleRow < _status[0].Count)
+                    {
+                        _status[HoleRow][HoleCol] = _status[HoleRow + 1][HoleCol];
+                        _status[HoleRow + 1][HoleCol] = 0;
+                        HoleRow++;
+                        return true;
+                    }
+                    else
+                        return false;
+
+                default:
+                    return false;
+
+            }
+        }
+
+        public void MoveOnceRandom()
+        {
+            Random random = new Random();
+            List<Key> list = new List<Key> { Key.Up, Key.Down, Key.Left, Key.Right };
+            for (int i = 4; true; i--)
+            {
+                Key dir = list[random.Next(0, i)];
+                if (MoveTile(dir) == true)
+                {
+                    return;
+                }
+            }
+        }
+
+        public void ShuffleTiles()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                MoveOnceRandom();
+            }
+        }
     }
 
-    
+
 }
