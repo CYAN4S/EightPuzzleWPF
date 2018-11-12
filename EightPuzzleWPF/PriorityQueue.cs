edfd94c1bf8163cc80748ab1e6c96f3988865c89
kumoh.computer <sih5214p@naver.com>
2018-11-12 11:46:36 +0900

    Redesign

---
+++ EightPuzzleWPF/PriorityQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzleWPF
+{
+    class PriorityQueue<T>
+    {
+        public PriorityQueue()
+        {
+
+        }
+
+        public void Enqueue(T t)
+        {
+
+        }
+
+        public T Dequeue()
+        {
+            return;
+        }
+    }
+}
